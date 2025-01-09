72a58d55519c1518697c667b743c49487d786101
chrisreddy04 <christurajreddy@gmail.com>
2025-01-09 14:43:15 -0500

    Changes to Resume Data

--- src/AboutSection.jsx
+++ src/AboutSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import ResumeFile from "./Resume/Christuraj_softwareDev_resume1.pdf";
+import ResumeFile from "./Resume/Christu_Resume_SoftDev.pdf";
 
 const AboutSection = () => {
   const [finished, setFinished] = useState(false);
