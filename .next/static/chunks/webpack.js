f04d4ba58c9f08a7e9824ef2d0426c8b95af26cd
chrisreddy04 <christurajreddy@gmail.com>
2025-02-26 18:50:32 -0500

    Trial img method

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e90900b37eb61669"; }
+/******/ 		__webpack_require__.h = function() { return "8ff2f3f7023a1755"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
